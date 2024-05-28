2d33f9f229733fd9de663b6af4a7fad8c98b7a00
C2mokete <137900736+C2mokete@users.noreply.github.com>
2024-05-28 21:42:34 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./features/ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./features/ui/AppLayout";
+import Error from "./features/ui/Error";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/Order/:orderId",
+        element: <Order />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
